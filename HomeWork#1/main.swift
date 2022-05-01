43444576c92de356ed78d49f0d4d9d2d2804b0af
Bibigul Aubakirova <bibigulaubakirova@Bibiguls-MacBook-Air.local>
2022-05-01 16:19:46 +0600

    Initial Commit

---
+++ HomeWork#1/main.swift
@@ -0,0 +1,50 @@
+//
+//  main.swift
+//  HomeWork#1
+//
+//  Created by Bibigul Aubakirova on 01.05.2022.
+//
+
+import Foundation
+
+var Rock: String = "Rock"
+var Paper: String = "Paper"
+var Scissors: String = "Scissors"
+
+func result() {
+    if let humanPick = readLine(){
+        if let machinePick = readLine(){
+            if (humanPick == Rock && machinePick == Paper) {
+                print("Ah, You Lost!")
+            } else if (humanPick == Rock && machinePick == Scissors) {
+                print("Nice, You won!")
+                
+            } else if (humanPick == Rock && machinePick == Rock) {
+                print("Oh it's a Draw!")
+                
+            } else if (humanPick == Paper && machinePick == Paper) {
+                print("Oh it's a Draw!")
+                
+            } else if (humanPick == Paper && machinePick == Scissors) {
+                print("Ah, You Lost!")
+                
+            } else if (humanPick == Paper && machinePick == Rock) {
+                print("Nice, You won!")
+                
+            } else if (humanPick == Scissors && machinePick == Paper) {
+                print("Nice, you won!")
+                
+            } else if (humanPick == Scissors && machinePick == Scissors) {
+                print("Oh it's a Draw!")
+                
+            } else if (humanPick == Scissors && machinePick == Rock) {
+                print("Ah, You Lost!")
+                
+            } else {
+                print("Please make your Choice")
+            }
+        }
+    }
+}
+
+result() // I would appreciate any feedback, comments
